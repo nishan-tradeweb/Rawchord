6e5792f7f05edbb4975e812622a25bc27b5364b5
nishan-tradeweb <mailstonishan@gmail.com>
2026-09-08 02:22:32 +0530

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -19,7 +19,7 @@ export const services = [
 ];
 
 export const works = [
-  { title: "Midnight Drive", artist: "RawChord Original", genre: "Electronic", audio: "" },
+  { title: "Kalyani", artist: "Jishakkul", genre: "Electronic", audio: "/Rawchord/music/KALYANI.mp3"},
   { title: "After Rain", artist: "Independent Artist", genre: "Ambient", audio: "" },
   { title: "Unsaid", artist: "RawChord Session", genre: "Pop", audio: "" },
   { title: "Velvet Sky", artist: "Independent Artist", genre: "R&B", audio: "" },
